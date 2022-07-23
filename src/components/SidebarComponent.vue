c5cfb11ab81786eb995f99fdb1e1b0874aa865ac
mohammad sarvi <mohamad.sarvi.1515@gmail.com>
2022-07-23 15:26:04 +0430

    bootstrap-sidebar

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,6 +1,44 @@
 <template>
   <div id="sidebar">
-    123
+      <aside>
+        <!-- Brand Logo -->
+        <div class="row brand-link p-5">
+          <div class="col-10 text-height">
+            <span class="brand-text fw-bold">پنل مدیریت فام724</span>
+          </div>
+          <div class="col-2">
+            <img src="../assets/styles/images/login-logo.png"  alt="AdminLTE Logo" class="brand-image img-circle">
+          </div>
+        </div>
+          <div class="ul-box mt-5">
+              <ul class="list-group">
+                <li class="list-group-item">
+                  <div class="row">
+                      <div class="col-2 d-flex justify-content-end">
+                        <img src="../assets/styles/images/menu-icons/dashboard-dark-icon.svg" alt="dashboard" class="">
+                      </div>
+                      <div class="col-10 ">
+                        <a class="">داشبورد</a>
+                      </div>
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  <div class="row">
+                    <div class="col-2 d-flex justify-content-end">
+                      <img src="../assets/styles/images/menu-icons/admin-light-icon.svg" alt="dashboard" class="">
+                    </div>
+                    <div class="col-10 ">
+                      <a class="">مدیریت کاربران پنل</a>
+                    </div>
+                  </div>
+                </li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+              </ul>
+          </div>
+        <!-- /.sidebar -->
+      </aside>
   </div>
 </template>
 
@@ -10,6 +48,6 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+@import "src/assets/styles/login";
 </style>
